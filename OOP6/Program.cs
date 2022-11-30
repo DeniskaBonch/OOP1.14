ebdb07184d03112f4ecbdad376eb22fc560b2d49
DeniskaBonch <115161192+DeniskaBonch@users.noreply.github.com>
2022-11-30 10:55:37 +0300

    wd

--- OOP6/Program.cs
+++ OOP6/Program.cs
@@ -14,7 +14,7 @@ namespace OOP6
             List<App> Arr = new List<App>()
             {
                 new App ("Nыyfjhаulд"),
-                new App ("Secoufkjgmnd"),
+                new App ("Secoufkdejgmnd"),
                 new App("Third")
             };
             foreach (App el in Arr)
